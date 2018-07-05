bce6214f8f390481577424ee86a19f491f64100a
Your NameMalaMare <marija.kovacic.bg@gmail.com>
2018-07-05 20:29:36 +0200

    initiation of triangle method

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+import trougao.Trougao;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Trougao trougao = new Trougao();
+        Scanner scanner = new Scanner(System.in);
+        
+//        for (int i = 0; i < 10; i++) {
+        
+        System.out.println("Unesite vrednost a:");
+        int a = scanner.nextInt();
+        trougao.setA(a);
+        
+        System.out.println("Unesite vrednost b:");
+        int b = scanner.nextInt();
+        trougao.setB(b);
+        
+        System.out.println("Unesite vrednost c");
+        int c = scanner.nextInt();
+        trougao.setC(c);
+        
+        
+        trougao.trougao();
+        
+//        System.out.println("");
+//        }
+        
+        
+    }               
+    
+}
---
+++ src/trougao/Trougao.java
@@ -0,0 +1,120 @@
+package trougao;
+
+public class Trougao {
+
+    private int a;
+    private int b;
+    private int c;
+
+    public Trougao() {
+        this.a = 1;
+        this.b = 1;
+        this.c = 1;
+    }
+
+    public Trougao(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public void trougao() {
+
+//        if (a+b<c || b+c<a || a+c<b) {
+//            System.out.println("Greska, zbir dve strane mora biti manji od trece");
+//        } if (a<0 || b<0 || c<0) {
+//            System.out.println("Greska, strane ne mogu biti negativni brojevi");
+//        } if (a==0 || b==0 || c==0) {
+//            System.out.println("Greska, strane ne mogu imati vrednost 0");
+//        } else if (a == b && b == c && a == c) {
+//            System.out.println("Trougao je jednakostranican ");
+//        } else if (a==b || a==c || b==c) {
+//            System.out.println("Trougao je jednakokraki");
+//        } else if (a!=b && b!=c && a!=c) {
+//            System.out.println("Trougao je nejednakostranican");
+//        }
+
+
+
+//        if ((a < 0 || b < 0 || c < 0) || (a + b < c || b + c < a || c + a < b)) {
+//            System.out.println("Podaci su neispravni");
+//        } else {
+//                if (a == b && b == c && c == a) {
+//                    System.out.println("Trougao je jednakostranican");
+//                } else if (a == b || b == c || c == a) {
+//                    System.out.println("Trougao je jednakokraki");
+//                } else if (a != b && b != c && a != c) {
+//                    System.out.println("Trougao je nejednakostranican");
+//                }
+//            }
+
+
+//        if (a > 0 && b > 0 && c > 0) {
+//            if (a + b > c && b + c > a && c + a > b) {
+//                if (a == b && b == c && c == a) {
+//                    System.out.println("Trougao je jednakostranican");
+//                } else if (a == b || b == c || c == a) {
+//                    System.out.println("Trougao je jednakokraki");
+//                } else if (a != b && b != c && a != c) {
+//                    System.out.println("Trougao je nejednakostranican");
+//                }
+//            } else {
+//                System.out.println("Neispravni parametri, zbir dve strane mora biti veci od trece");
+//            }
+//
+//        } else {
+//            System.out.println("Neispravni parametri, moraju biti >0");
+//
+//        }
+
+
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            System.out.println("Neispravni parametri, moraju biti >0");
+            if (a + b < c || b + c < a || c + a < b) {
+                System.out.println("Neispravni parametri, zbir dve strane mora biti veci od trece");
+            }
+        }  
+        else {if (a==b && b !=c ) {
+              System.out.println("Trougao je jednakokraki");
+              }
+              else if (b==c && c!=a) {
+              System.out.println("Trougao je jednakokraki");    
+              }
+              else if (c==a && a!=b) {
+              System.out.println("Trougao je jednakokraki");    
+              }
+              else if (a == b && b == c && c == a) {
+              System.out.println("Trougao je jednakostranican");
+              }
+              else if  (a != b && b != c && a != c) {
+              System.out.println("Trougao je nejednakostranican");
+              }
+        }
+
+    }
+}
